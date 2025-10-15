b4e73a6ad239ce75e24d065631b62c210318b266
Anshul Raj S V <110563622+Anshul-Raj-S-V@users.noreply.github.com>
2025-10-15 14:49:42 +0530

    Update application.py

--- application.py
+++ application.py
@@ -41,6 +41,9 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+application = app
+
+if __name__ == "__main__":
+    # For local testing
+    app.run(host="0.0.0.0", port=8080)        
 
